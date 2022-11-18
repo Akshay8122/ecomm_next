4ee323295c2ece88c994e01ae55cb66c1f2a2bc1
Akshay <akshay.p@simformsolutions.com>
2022-11-18 15:17:04 +0530

    feat:added Herobanner component & modify changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,28 @@
+import React from "react";
+import { client } from "../lib/client";
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
+import {
+  Cart,
+  Footer,
+  FooterBanner,
+  HeroBanner,
+  Layout,
+  Navbar,
+  Product,
+} from "../components";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <>
-      HeroBanner
+    <div>
+      <HeroBanner />
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
       <div>{["Product 1", "Product 2"].map((product) => product)}</div>
-      Footer
-    </>
+      <FooterBanner />
+    </div>
   );
 };
 
