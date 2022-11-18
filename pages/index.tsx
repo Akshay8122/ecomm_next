90ade5537fce2f927cff20a327135fd3517a3efa
Akshay <akshay.p@simformsolutions.com>
2022-11-18 18:28:21 +0530

    feat:minor changes added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { client } from "../lib/client";
-import type { NextPage } from "next";
 import {
   Cart,
   Footer,
@@ -11,19 +10,41 @@ import {
   Product,
 } from "../components";
 import styles from "../styles/Home.module.css";
+import { NextPage } from "next";
 
-const Home: NextPage = () => {
-  return (
-    <div>
-      <HeroBanner />
-      <div className="products-heading">
-        <h2>Best Selling Products</h2>
-        <p>Speakers of many variations</p>
-      </div>
-      <div>{["Product 1", "Product 2"].map((product) => product)}</div>
-      <FooterBanner />
+interface HomeProps {
+  products: any[];
+  bannerData: any[];
+}
+
+const Home = ({ products, bannerData }: HomeProps) => (
+  <div>
+    <HeroBanner />
+    {/* {console.log(bannerData)} */}
+    <div className="products-heading">
+      <h2>Best Selling Products</h2>
+      <p>Speakers of many variations</p>
     </div>
-  );
+    <div className="products-container">
+      {products?.map((items) => items.name)}
+    </div>
+    <FooterBanner />
+  </div>
+);
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products: string = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: {
+      products,
+      bannerData,
+    },
+  };
 };
 
 export default Home;
