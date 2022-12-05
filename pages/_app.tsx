4568d9db5c2893284bcc99da8ca47cd784e9741b
Akshay <patelakshay43751@gmail.com>
2022-12-05 18:40:15 +0530

    feat:wrap all component into context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import "../styles/globals.css";
+import { Toaster } from "react-hot-toast";
 import type { AppProps } from "next/app";
 import { Layout } from "../components";
+import { StateContext } from "../context/StateContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
   );
 }
 
