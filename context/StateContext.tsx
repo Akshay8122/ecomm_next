0f35ad0fd4a81df620e804b86ebebece9515d824
Akshay <patelakshay43751@gmail.com>
2022-12-08 18:09:04 +0530

    feat:Assign type to the StateContext component

--- context/StateContext.tsx
+++ context/StateContext.tsx
@@ -1,32 +1,34 @@
-import React, { createContext, useContext, useState, useEffect } from "react";
+import React, { createContext, useContext, useState } from "react";
 import { toast } from "react-hot-toast";
+import { IStateContext } from "../typing";
 
-const Context = createContext<any | null>(null);
+const Context = createContext<any>(null);
 
-export const StateContext = ({ children }: any) => {
-  const [showCart, setShowCart] = useState<boolean>(false);
-  const [cartItems, setCartItems] = useState<any>([]);
-  const [totalPrice, setTotalPrice] = useState(0);
+export const StateContext = ({ children }: { children: React.ReactNode }) => {
+  const [showCart, setShowCart] = useState(false);
+  const [cartItems, setCartItems] = useState<IStateContext[]>([]);
+  const [totalPrice, setTotalPrice] = useState<number>(0);
   const [totalQuantities, setTotalQuantities] = useState(0);
   const [qty, setQty] = useState(1);
 
   let foundProduct: any;
   let index;
 
-  const onAdd = (product: any, quantity: number) => {
+  const onAdd = (product: IStateContext, quantity: number) => {
     const checkProductInCart = cartItems.find(
       (item: any) => item._id === product._id
     );
 
     setTotalPrice(
-      (prevTotalPrice: any) => prevTotalPrice + product.price * quantity
+      (prevTotalPrice: number) => prevTotalPrice + product.price * quantity
     );
     setTotalQuantities(
-      (prevTotalQuantities: any) => prevTotalQuantities + quantity
+      (prevTotalQuantities: number) => prevTotalQuantities + quantity
     );
 
     if (checkProductInCart) {
-      const updatedCartItems = cartItems.map((cartProduct: any) => {
+      const updatedCartItems: any = cartItems.map((cartProduct) => {
+        console.log("this is for cartItems", cartProduct);
         if (cartProduct._id === product._id)
           return {
             ...cartProduct,
@@ -42,11 +44,9 @@ export const StateContext = ({ children }: any) => {
     toast.success(`${qty} ${product.name} is Added to the cart`);
   };
 
-  const onRemove = (product: any) => {
-    foundProduct = cartItems.find((item: any) => item._id === product._id);
-    const newCartItems = cartItems.filter(
-      (item: any) => item._id !== product._id
-    );
+  const onRemove = (product: IStateContext) => {
+    foundProduct = cartItems.find((item) => item._id === product._id);
+    const newCartItems = cartItems.filter((item) => item._id !== product._id);
 
     setTotalPrice(
       (prevTotalPrice) =>
@@ -59,9 +59,9 @@ export const StateContext = ({ children }: any) => {
   };
 
   const toggleCartItemQuantity = (id: number, value: any) => {
-    foundProduct = cartItems.find((item: any) => item._id === id);
-    index = cartItems.findIndex((product: any) => product._id === id);
-    const newCartItems = cartItems.filter((item: any) => item._id !== id);
+    foundProduct = cartItems.find((item) => item._id === id);
+    index = cartItems.findIndex((product) => product._id === id);
+    const newCartItems = cartItems.filter((item) => item._id !== id);
 
     if (value === "inc") {
       setCartItems([
