89c340a0afa1b0d299f71267a17c81ad134aab2e
Akshay <patelakshay43751@gmail.com>
2022-12-12 18:44:23 +0530

    feat:Made minor changes

--- pages/product/[slug].tsx
+++ pages/product/[slug].tsx
@@ -29,6 +29,7 @@ const ProductDetails = ({ product, products }: ISlugDetails) => {
           <div className="small-images-container">
             {image?.map((item: string, i: number) => (
               <img
+                key={i}
                 src={urlFor(item)}
                 className={
                   i == index ? "small-image selected-image" : "small-image"
